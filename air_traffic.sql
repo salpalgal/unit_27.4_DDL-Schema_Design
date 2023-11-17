3c1fdeacdc53b6bd7d7bd3bc334fc43c3486a391
sally wang <sal@LAPTOP-66V3CAS9.localdomain>
2023-11-17 09:59:04 -0800

    part 2: Schema Design

--- air_traffic.sql
+++ air_traffic.sql
@@ -7,6 +7,44 @@ CREATE DATABASE air_traffic;
 
 \c air_traffic
 
+
+  CREATE TABLE airlines
+  (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+  );
+
+  INSERT INTO airlines
+  (name)
+  VALUES
+  ('United'),
+  ('British Airway'),
+  ('Delta'),
+  ('TUI Fly Belgium'),
+  ('Air China'),
+  ('American Airlines'),
+  ('Avianca Brasil');
+
+  CREATE TABLE countries 
+  (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+  );
+
+  INSERT INTO countries 
+  (name)
+  VALUES
+  ('United States'),
+  ('Japan'),
+  ('France'),
+  ('UAE'),
+  ('Brazil'),
+  ('United Kingdom'),
+  ('Mexico'),
+  ('Morocco'),
+  ('China'),
+  ('Chile');
+
 CREATE TABLE tickets
 (
   id SERIAL PRIMARY KEY,
@@ -15,23 +53,24 @@ CREATE TABLE tickets
   seat TEXT NOT NULL,
   departure TIMESTAMP NOT NULL,
   arrival TIMESTAMP NOT NULL,
-  airline TEXT NOT NULL,
+  airline_id INTEGER REFERENCES airlines ON DELETE CASCADE,
   from_city TEXT NOT NULL,
-  from_country TEXT NOT NULL,
+  from_country_id INTEGER REFERENCES countries ON DELETE CASCADE,
   to_city TEXT NOT NULL,
-  to_country TEXT NOT NULL
+  to_country_id INTEGER REFERENCES countries ON DELETE CASCADE
 );
 
 INSERT INTO tickets
-  (first_name, last_name, seat, departure, arrival, airline, from_city, from_country, to_city, to_country)
+  (first_name, last_name, seat, departure, arrival, airline_id, from_city, from_country_id, to_city, to_country_id)
 VALUES
-  ('Jennifer', 'Finch', '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 'United', 'Washington DC', 'United States', 'Seattle', 'United States'),
-  ('Thadeus', 'Gathercoal', '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00', 'British Airways', 'Tokyo', 'Japan', 'London', 'United Kingdom'),
-  ('Sonja', 'Pauley', '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00', 'Delta', 'Los Angeles', 'United States', 'Las Vegas', 'United States'),
-  ('Jennifer', 'Finch', '20A', '2018-04-15 16:50:00', '2018-04-15 21:00:00', 'Delta', 'Seattle', 'United States', 'Mexico City', 'Mexico'),
-  ('Waneta', 'Skeleton', '23D', '2018-08-01 18:30:00', '2018-08-01 21:50:00', 'TUI Fly Belgium', 'Paris', 'France', 'Casablanca', 'Morocco'),
-  ('Thadeus', 'Gathercoal', '18C', '2018-10-31 01:15:00', '2018-10-31 12:55:00', 'Air China', 'Dubai', 'UAE', 'Beijing', 'China'),
-  ('Berkie', 'Wycliff', '9E', '2019-02-06 06:00:00', '2019-02-06 07:47:00', 'United', 'New York', 'United States', 'Charlotte', 'United States'),
-  ('Alvin', 'Leathes', '1A', '2018-12-22 14:42:00', '2018-12-22 15:56:00', 'American Airlines', 'Cedar Rapids', 'United States', 'Chicago', 'United States'),
-  ('Berkie', 'Wycliff', '32B', '2019-02-06 16:28:00', '2019-02-06 19:18:00', 'American Airlines', 'Charlotte', 'United States', 'New Orleans', 'United States'),
-  ('Cory', 'Squibbes', '10D', '2019-01-20 19:30:00', '2019-01-20 22:45:00', 'Avianca Brasil', 'Sao Paolo', 'Brazil', 'Santiago', 'Chile');
+  ('Jennifer', 'Finch', '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 1, 'Washington DC', 1, 'Seattle', 1),
+  ('Thadeus', 'Gathercoal', '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00', 2, 'Tokyo', 2, 'London', 6),
+  ('Sonja', 'Pauley', '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00', 3, 'Los Angeles', 1, 'Las Vegas', 1),
+  ('Jennifer', 'Finch', '20A', '2018-04-15 16:50:00', '2018-04-15 21:00:00', 3, 'Seattle', 1, 'Mexico City', 7),
+  ('Waneta', 'Skeleton', '23D', '2018-08-01 18:30:00', '2018-08-01 21:50:00', 4, 'Paris', 3, 'Casablanca', 8),
+  ('Thadeus', 'Gathercoal', '18C', '2018-10-31 01:15:00', '2018-10-31 12:55:00', 5, 'Dubai', 4, 'Beijing', 9),
+  ('Berkie', 'Wycliff', '9E', '2019-02-06 06:00:00', '2019-02-06 07:47:00', 1, 'New York', 1, 'Charlotte', 1),
+  ('Alvin', 'Leathes', '1A', '2018-12-22 14:42:00', '2018-12-22 15:56:00', 6, 'Cedar Rapids', 1, 'Chicago', 1),
+  ('Berkie', 'Wycliff', '32B', '2019-02-06 16:28:00', '2019-02-06 19:18:00', 6, 'Charlotte', 1, 'New Orleans',1),
+  ('Cory', 'Squibbes', '10D', '2019-01-20 19:30:00', '2019-01-20 22:45:00', 7, 'Sao Paolo', 5, 'Santiago', 10);
+
--- outer_space.sql
+++ outer_space.sql
@@ -7,22 +7,106 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
+
+CREATE TABLE orbits_around 
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+INSERT INTO orbits_around 
+(name)
+VALUES
+('The Sun'),
+('Proxima Centauri'),
+('Gliese 876');
+
+CREATE TABLE galaxy 
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+INSERT INTO galaxy
+(name)
+VALUES
+('Milky Way');
+
+CREATE TABLE moons 
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
 CREATE TABLE planets
 (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  orbits_around_id INTEGER REFERENCES orbits_around ON DELETE CASCADE,
+  galaxy_id INTEGER REFERENCES galaxy ON DELETE CASCADE
+
 );
 
 INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+(name, orbital_period_in_years, orbits_around_id, galaxy_id)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('Earth', 1.00, 1, 1),
+  ('Mars', 1.88, 1, 1),
+  ('Venus', 0.62, 1, 1),
+  ('Neptune', 164.8, 1, 1),
+  ('Proxima Centauri b', 0.03, 2, 1),
+  ('Gliese 876 b', 0.23, 3, 1);
+
+
+
+INSERT INTO moons
+(name)
+VALUES
+('The Moon'),
+('Phobos'),
+('Deimos'),
+('Naiad'),
+('Thalassa'),
+('Despina'),
+('Galatea'),
+('Larissa'),
+('S/2004 N 1'),
+('"Proteus'),
+('Triton'),
+('Nereid'),
+('Halimede'),
+('Sao'),
+('Laomedeia'),
+('Psamathe'),
+('Neso');
+
+CREATE TABLE planets_moons 
+(
+  id SERIAL PRIMARY KEY,
+  planet_id INTEGER REFERENCES planets ON DELETE CASCADE,
+  moon_id INTEGER REFERENCES moons ON DELETE CASCADE
+);
+
+INSERT INTO planets_moons
+(planet_id, moon_id)
+VALUES
+(1,1),
+(2,2),
+(2,3),
+(4,4),
+(4,5),
+(4,6),
+(4,7),
+(4,8),
+(4,9),
+(4,10),
+(4,11),
+(4,12),
+(4,13),
+(4,14),
+(4,15),
+(4,16),
+(4,17);
+
+
+
